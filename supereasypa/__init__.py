0ee1457a9d3c7a654a3e644a3f6fd7e9249a1655
Matthew Irving <matt@develop-matt.com>
2020-07-02 17:52:17 -0400

    git push

--- supereasypa/__init__.py
+++ supereasypa/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from supereasypa import app
+
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecret'
 
